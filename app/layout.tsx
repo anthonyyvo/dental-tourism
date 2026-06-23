57702fc1955e2a673f4c8d353dd3a5fb39edc1b7
Hoai An <anthonyvominh@gmail.com>
2026-06-24 00:03:27 +0700

    update-wg

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           strategy="afterInteractive"
           data-org="69d0e69cb452b246b49d00ba"
           data-channel="6a3a1df6e7507be3d1f4408d"
+          data-api="https://dental-crm-beta-v1-0.onrender.com"
         />
+        
         {/* <Script id="custom-inline-script" strategy="afterInteractive">
           {`
 !function(s,u,b,i,z){var o,t,r,y;s[i]||(s._sbzaccid=z,s[i]=function(){s[i].q.push(arguments)},s[i].q=[],s[i]("setAccount",z),r=["widget.subiz.net","storage.googleapis"+(t=".com"),"app.sbz.workers.dev",i+"a"+(o=function(k,t){var n=t<=6?5:o(k,t-1)+o(k,t-3);return k!==t?n:n.toString(32)})(20,20)+t,i+"b"+o(30,30)+t,i+"c"+o(40,40)+t],(y=function(k){var t,n;s._subiz_init_2094850928430||r[k]&&(t=u.createElement(b),n=u.getElementsByTagName(b)[0],t.async=1,t.src="https://"+r[k]+"/sbz/app.js?accid="+z,n.parentNode.insertBefore(t,n),setTimeout(y,2e3,k+1))})(0))}(window,document,"script","subiz", "acrwuznfnxprpjyakhof") `}
